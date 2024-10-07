4c1586cc8a50d1dff4c532597719e1d0b6360e2b
cloudyrider <79820393+cloudyrider@users.noreply.github.com>
2024-10-07 21:02:55 +0900

    Title: 색깔 트리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241007/색깔 트리/color-tree.py
+++ 241007/색깔 트리/color-tree.py
@@ -26,55 +26,49 @@ class Node:
         for child in self.children:
             child.change_color(color)
 
-    def get_value(self, colors) :
+    def get_value(self, colors):
         colors.add(self.color)
-        for child in self.children :
+        for child in self.children:
             child.get_value(colors)
 
 nodes = {}
 roots = []
 
-for order in orders :
-    #노드추가
-    #100 m_id p_id color max_depth
-    if order[0] == 100 : 
+for order in orders:
+    # 노드 추가
+    if order[0] == 100: 
         _, m_id, p_id, color, max_depth = order
-
         node = Node(m_id, p_id, color, max_depth)
 
-        if p_id == -1 :
+        if p_id == -1:
             roots.append(m_id)
             nodes[m_id] = node
-        else :
+        else:
             parent = nodes[p_id]
-            # 문제조건 : m_id 는 새로 추가되는 노드 번호로 항상 새로운 값이 주어지며, p_id는 −1 이 아닌 이상 항상 이미 주어진 노드 번호가 주어짐을 가정해도 좋습니다.
-
-            if parent.max_depth > 1 :
+            # p_id는 항상 이미 존재하는 노드라고 가정
+            if parent.max_depth > 1:
                 nodes[m_id] = node
                 parent.add_child(node)
                 parent.max_depth -= 1
 
-    #색깔 변경
-    #200 m_id color
-    elif order[0] == 200 : 
+    # 색깔 변경
+    elif order[0] == 200: 
         _, m_id, color = order
         nodes[m_id].change_color(color)
 
-    #색깔 조회
-    #300 m_id
-    elif order[0] == 300 : 
+    # 색깔 조회
+    elif order[0] == 300: 
         _, m_id = order
         print(nodes[m_id].color)
 
-    #점수 조회
-    #이 경우 모든 노드의 가치를 계산하여, 가치 제곱의 합을 출력합니다.
-    #각 노드의 가치는 해당 노드를 루트로 하는 서브트리 내 서로 다른 색깔의 수로 정의됩니다.
-    elif order[0] == 400 : 
+    # 점수 조회
+    elif order[0] == 400: 
         value = 0
 
-        for node in nodes.values()
+        # 모든 노드에 대해 계산
+        for node in nodes.values():  # 수정: values()로 변경
             colors = set()
             node.get_value(colors)
-            value += len(colors)**2
+            value += len(colors) ** 2
         
         print(value)
