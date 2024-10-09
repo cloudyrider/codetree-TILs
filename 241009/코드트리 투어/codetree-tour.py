4bfffe5c43b9d3cfbde8fcd312aeeec27dcf8fd3
cloudyrider <79820393+cloudyrider@users.noreply.github.com>
2024-10-09 18:41:23 +0900

    Title: 코드트리 투어, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241009/코드트리 투어/codetree-tour.py
+++ 241009/코드트리 투어/codetree-tour.py
@@ -4,32 +4,47 @@ orders = []
 for _ in range(Q) :
     orders.append(list(map(int, input().split())))
 
-dep = 0
-packages = {}
+def dijkstra(deps, dests, world) :
+    costs = {}
 
-def dijkstra(dep, world):
-    dist = [float('inf')] * len(world)
-    dist[dep] = 0
-    visited = [False] * len(world)
+    for dep in deps :
+        dests_ = dests
+        temp_costs = [float('inf')] * len(world)
+        temp_costs[dep] = 0
+        visited = [False] * len(world)
 
-    for _ in range(len(world)):
-        min_dist = float('inf')
-        min_node = -1
-        for i in range(len(world)):
-            if not visited[i] and dist[i] < min_dist:
-                min_dist = dist[i]
-                min_node = i
-        if min_node == -1:
-            break
-        visited[min_node] = True
-        for nei, cost in world[min_node]:
-            if dist[min_node] + cost < dist[nei]:
-                dist[nei] = dist[min_node] + cost
+        for _ in range(len(world)) :
+            min_cost = float('inf')
+            which_min = -1
 
-    return dist
+            for i in range(len(world)) :
+                if not visited[i] :
+                    if temp_costs[i] < min_cost :
+                        min_cost = temp_costs[i]
+                        which_min = i
+            
+            if which_min == -1 :
+                break
     
+            visited[which_min] = True
+            if which_min in dests_ :
+                dests_.remove(which_min)
+
+            if not dests_ :
+                break
+            
+            for nei, cost in world[which_min]:
+                if nei != which_min : 
+                    if temp_costs[which_min] + cost < temp_costs[nei]:
+                        temp_costs[nei] = temp_costs[which_min] + cost
+
+        costs[dep] = temp_costs
+
+    return costs
+
+dests = []
+
 for order in orders :
-    #print(order)
     if order[0] == 100 :
         n, m = order[1], order[2]
         world = {i:set() for i in range(n)}
@@ -39,7 +54,24 @@ for order in orders :
             world[v].add((u, w))
             world[u].add((v, w))
             idx += 3
-        costs = dijkstra(dep, world)
+        break
+
+deps = set()
+deps.add(0)
+dests = set()
+
+for order in orders :
+    if order[0] == 200 :
+        dests.add(order[3])
+
+    elif order[0] == 500 :
+        deps.add(order[1])
+
+costs = dijkstra(deps, dests, world)
+dep = 0
+packages = {}
+
+for order in orders :
 
     if order[0] == 200 :
         id_, revenue, dest = order[1], order[2], order[3]
@@ -51,26 +83,24 @@ for order in orders :
             del packages[id_]
 
     if order[0] == 400 :
+        costs_from_dep_to = costs[dep]
         max_income = -1
-        that_id = 0
+        max_id = -1
+
         for id_, value in packages.items() :
             revenue, dest = value
-            if costs[dest] == float('inf') :
-                continue
-            if revenue - costs[dest] > max_income :
-                max_income = revenue - costs[dest]
-                that_id = id_
-            elif revenue - costs[dest] == max_income :
-                if that_id > id_ :
-                    that_id = id_
-
+            income = revenue - costs_from_dep_to[dest]
+            if income > max_income :
+                max_income = income
+                max_id = id_
+            elif income == max_income :
+                max_id = min(id_, max_id)
+         
         if max_income > -1 :
-            print(that_id)
-            del packages[that_id]
+            print(max_id)
+            del packages[max_id]
         else :
             print(-1)
 
     if order[0] == 500 :
-        s = order[1]
-        dep = s
-        costs = dijkstra(dep, world)
+        dep = order[1]
